a33ca9347914173ea53b6a06fe65af39b1245e7c
Pere Urbon <pere@confluent.io>
2019-05-04 15:35:44 +0200

    add params to the topology parser

--- src/main/scala/Topology.scala
+++ src/main/scala/Topology.scala
@@ -1,6 +1,6 @@
 case class Projects(projects: Array[Project]);
 
-case class Project(name: String, users: Accounts, topics: Array[ProjectTopic]=Array.empty);
+case class Project(name: String, users: Accounts, topics: Array[ProjectTopic]=Array.empty, zookeepers: Array[String]=Array("localhost:2181"));
 
 case class Accounts(consumers: Array[String], producers: Array[String], streams: Array[KStreamApp], connectors: Array[String] )
 
