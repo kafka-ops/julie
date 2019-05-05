60cc53461330d9d096331720888fe9fbfb73b314
Pere Urbon-Bayes <pere.urbon@gmail.com>
2019-05-05 10:31:00 +0200

    add cli parser options

--- build.sbt
+++ build.sbt
@@ -4,6 +4,12 @@ version := "0.1"
 
 scalaVersion := "2.12.8"
 
+resolvers += "Sonatype Nexus releases" at "https://oss.sonatype.org/content/repositories/releases"
+
+
+libraryDependencies += "org.sellmerfud" %% "optparse" % "2.2"
+
 libraryDependencies += "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.9.8"
 libraryDependencies += "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml" % "2.9.8"
 
+mainClass in assembly := Some("TopologyBuilder")
--- src/main/scala/TopologyBuilder.scala
+++ src/main/scala/TopologyBuilder.scala
@@ -3,15 +3,23 @@ import java.io.FileReader
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import org.sellmerfud.optparse.OptionParser
 
 class TopologyBuilder {
 
 }
 
+case class Config(descriptorFile: String = "")
+
 object TopologyBuilder {
 
   val mapper: ObjectMapper = new ObjectMapper(new YAMLFactory())
   mapper.registerModule(DefaultScalaModule)
+
+  val cli = new OptionParser[Config] {
+    reqd[String]("-f", "--file STRING", "filename") { (value, cfg) => cfg.copy(descriptorFile = value) }
+  }
+
 
   def parse(file: String): Projects = {
     mapper.readValue(new FileReader(file), classOf[Projects]);
@@ -19,8 +27,9 @@ object TopologyBuilder {
 
   def main(args: Array[String]): Unit = {
 
+    val config: Config = cli.parse(args, Config())
     val cmd = new PrepareCommand()
-    val projects = parse("descriptor.yaml")
+    val projects = parse(config.descriptorFile)
     cmd.execute(projects)
   }
 }
