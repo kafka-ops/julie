b8ebc19ab85cbc0e39dc67ae14ea9f0435b0dc70
Pere Urbon-Bayes <pere.urbon@gmail.com>
2019-10-20 10:51:34 +0200

    add test for topic update

--- src/test/java/com/purbon/kafka/topology/TopicManagerTest.java
+++ src/test/java/com/purbon/kafka/topology/TopicManagerTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.when;
 import com.purbon.kafka.topology.model.Project;
 import com.purbon.kafka.topology.model.Topic;
 import com.purbon.kafka.topology.model.Topology;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import org.apache.kafka.clients.admin.KafkaAdminClient;
@@ -51,5 +52,26 @@ public class TopicManagerTest {
 
     verify(adminClient, times(1)).createTopic(topicA, topicA.composeTopicName(topology, project.getName()));
     verify(adminClient, times(1)).createTopic(topicB, topicB.composeTopicName(topology, project.getName()));
+  }
+
+  @Test
+  public void newTopicUpdateTest() {
+
+    Project project = new Project("project");
+    Topic topicA = new Topic("topicA");
+    project.addTopic(topicA);
+    Topic topicB = new Topic("topicB");
+    project.addTopic(topicB);
+    Topology topology = new Topology();
+    topology.addProject(project);
+
+    Set<String> dummyTopicList = new HashSet<>();
+    dummyTopicList.add(topicB.composeTopicName(topology, project.getName()));
+    when(adminClient.listTopics()).thenReturn(dummyTopicList);
+
+    topicManager.syncTopics(topology);
+
+    verify(adminClient, times(1)).createTopic(topicA, topicA.composeTopicName(topology, project.getName()));
+    verify(adminClient, times(1)).updateTopicConfig(topicB, topicB.composeTopicName(topology, project.getName()));
   }
 }
