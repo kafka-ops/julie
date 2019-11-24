45c6a3977da24c7c6f20fb5719fdead9b51400f7
Pere Urbon-Bayes <pere.urbon@gmail.com>
2019-11-24 16:20:46 +0100

    introduce the idea of data types to the topic name

---
+++ src/test/java/com/purbon/kafka/topology/TopicTest.java
@@ -0,0 +1,47 @@
+package com.purbon.kafka.topology;
+
+import com.purbon.kafka.topology.model.Project;
+import com.purbon.kafka.topology.model.Topic;
+import com.purbon.kafka.topology.model.Topology;
+import java.util.Arrays;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TopicTest {
+
+  Topology topology;
+  Project project;
+
+  @Before
+  public void before() {
+    topology = new Topology();
+    project = new Project();
+    topology.setSource("source");
+    topology.setTeam("team");
+
+    project.setName("project");
+    topology.setProjects(Arrays.asList(project));
+  }
+
+  @Test
+  public void buildTopicNameTest() {
+    Topic topic = new Topic("topic");
+    String fulllName = topic.composeTopicName(topology, project);
+    Assert.assertEquals("team.source.project.topic", fulllName);
+
+    fulllName = topic.composeTopicName(topology, project.getName());
+    Assert.assertEquals("team.source.project.topic", fulllName);
+  }
+
+  @Test
+  public void buildTopicNameWithDataTypeTest() {
+    Topic topic = new Topic("topic", "type");
+    String fulllName = topic.composeTopicName(topology, project);
+    Assert.assertEquals("team.source.project.topic.type", fulllName);
+
+    fulllName = topic.composeTopicName(topology, project.getName());
+    Assert.assertEquals("team.source.project.topic.type", fulllName);
+  }
+
+}
