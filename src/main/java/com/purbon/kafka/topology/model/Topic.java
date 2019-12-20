15d7cb95e9854cad93a68437fc9c28be00ee503a
Pere Urbon <pere@confluent.io>
2019-12-20 13:06:34 +0100

    make the datatype field as optional

--- src/main/java/com/purbon/kafka/topology/model/Topic.java
+++ src/main/java/com/purbon/kafka/topology/model/Topic.java
@@ -1,35 +1,38 @@
 package com.purbon.kafka.topology.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.purbon.kafka.topology.TopicManager;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class Topic {
 
-  public static final String UNKNOWN_DATATYPE = "unknown";
   public static final String DEFAULT_TOPIC_NAME = "default";
-  private String dataType;
+  @JsonInclude(Include.NON_EMPTY)
+  private Optional<String> dataType;
   private String name;
   private HashMap<String, String> config;
 
   private Project project;
 
   public Topic(String name) {
-    this(name, UNKNOWN_DATATYPE, new HashMap<>());
+    this(name, Optional.empty(), new HashMap<>());
   }
 
   public Topic(String name, String dataType) {
-    this(name, dataType, new HashMap<>());
+    this(name, Optional.of(dataType), new HashMap<>());
   }
 
-  public Topic(String name, String dataType, HashMap<String, String> config) {
+  public Topic(String name, Optional<String> dataType, HashMap<String, String> config) {
     this.name = name;
     this.dataType = dataType;
     this.config = config;
   }
 
   public Topic() {
-    this(DEFAULT_TOPIC_NAME, UNKNOWN_DATATYPE, new HashMap<>());
+    this(DEFAULT_TOPIC_NAME, Optional.empty(), new HashMap<>());
   }
 
   public String getName() {
@@ -42,9 +45,9 @@ public class Topic {
         .append(".")
         .append(getName());
 
-    if (!getDataType().equals(UNKNOWN_DATATYPE)) {
+    if (getDataType().isPresent()) {
       sb.append(".")
-          .append(getDataType());
+          .append(getDataType().get());
     }
 
     return sb.toString();
@@ -73,7 +76,7 @@ public class Topic {
     return getConfig();
   }
 
-  public String getDataType() {
+  public Optional<String> getDataType() {
     return dataType;
   }
 
