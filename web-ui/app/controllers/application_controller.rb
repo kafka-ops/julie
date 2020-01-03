ce6dae8d4f464a8ea7c5d9006680eea37879d579
Pere Urbon <pere@confluent.io>
2020-01-03 17:21:17 +0100

    add basic topology builder

--- web-ui/app/controllers/application_controller.rb
+++ web-ui/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || topologies_path
+  end
+
 end
--- web-ui/app/controllers/topologies_controller.rb
+++ web-ui/app/controllers/topologies_controller.rb
@@ -1,5 +1,8 @@
 class TopologiesController < ApplicationController
 
+  def index
+  end
+  
   def new
     @topology = Topology.new
   end
