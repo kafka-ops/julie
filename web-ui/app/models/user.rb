2994e8f128e57bc63a836aba0bcdfbc5f2d68cc2
Pere Urbon <pere@confluent.io>
2020-01-03 16:34:56 +0100

    Revert "simply user login form"
    This reverts commit 64ac190e4ab2886330df99969656f8d8d6a3ba55.

--- web-ui/app/models/user.rb
+++
@@ -1,3 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-end
--- web-ui/config/routes.rb
+++ web-ui/config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-
-  resources :users, only: [:new, :create]
-  get  'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  get  'welcome', to: 'sessions#welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "main#index"
