a3c40e2f8b401292b765f215e9f0bfa15425d177
Pere Urbon-Bayes <pere.urbon@gmail.com>
2020-02-22 08:34:53 +0100

    Add access control roles for connect and streams using rbac

--- src/main/java/com/purbon/kafka/topology/roles/RBACPredefinedRoles.java
+++ src/main/java/com/purbon/kafka/topology/roles/RBACPredefinedRoles.java
@@ -6,4 +6,5 @@ public class RBACPredefinedRoles {
   public static final String DEVELOPER_WRITE = "DeveloperWrite";
 
   public static final String RESOURCE_OWNER = "ResourceOwner";
+  public static final String DEVELOPER_MANAGE = "DeveloperManage";
 }
--- src/main/java/com/purbon/kafka/topology/roles/RBACProvider.java
+++ src/main/java/com/purbon/kafka/topology/roles/RBACProvider.java
@@ -1,16 +1,21 @@
 package com.purbon.kafka.topology.roles;
 
+import static com.purbon.kafka.topology.roles.RBACPredefinedRoles.DEVELOPER_MANAGE;
 import static com.purbon.kafka.topology.roles.RBACPredefinedRoles.DEVELOPER_READ;
 import static com.purbon.kafka.topology.roles.RBACPredefinedRoles.DEVELOPER_WRITE;
+import static com.purbon.kafka.topology.roles.RBACPredefinedRoles.RESOURCE_OWNER;
 
 import com.purbon.kafka.topology.AccessControlProvider;
 import com.purbon.kafka.topology.api.mds.MDSApiClient;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class RBACProvider implements AccessControlProvider {
 
   public static final String LITERAL = "LITERAL";
+  public static final String PREFIX = "PREFIX";
   private final MDSApiClient apiClient;
 
   public RBACProvider(MDSApiClient apiClient) {
@@ -25,11 +30,37 @@ public class RBACProvider implements AccessControlProvider {
   @Override
   public void setAclsForConnect(String principal, String topicPrefix, List<String> readTopics, List<String> writeTopics) {
 
+    apiClient.bind(principal, DEVELOPER_READ, topicPrefix, PREFIX);
+    readTopics.forEach(topic -> apiClient.bind(principal, DEVELOPER_READ, topic, LITERAL));
+    writeTopics.forEach(topic -> apiClient.bind(principal, DEVELOPER_WRITE, topic, LITERAL));
+
+    String[] resources = new String[]{"Topic:connect-configs",
+        "Topic:connect-offsets",
+        "Topic:connect-status",
+        "Group:connect-cluster",
+        "Group:secret-registry",
+        "Topic:_confluent-secrets"};
+
+    Arrays
+        .asList(resources)
+        .forEach(resourceObject -> {
+          String[] elements = resourceObject.split(":");
+          String resource = elements[0];
+          String resourceType = elements[1];
+          apiClient.bind(principal, RESOURCE_OWNER, resource, resourceType, LITERAL);
+        });
   }
 
   @Override
   public void setAclsForStreamsApp(String principal, String topicPrefix, List<String> readTopics,
       List<String> writeTopics) {
+
+    apiClient.bind(principal, DEVELOPER_READ, topicPrefix, PREFIX);
+    readTopics.forEach(topic -> apiClient.bind(principal, DEVELOPER_READ, topic, LITERAL));
+    writeTopics.forEach(topic -> apiClient.bind(principal, DEVELOPER_WRITE, topic, LITERAL));
+
+    apiClient.bind(principal, RESOURCE_OWNER, topicPrefix, PREFIX);
+    apiClient.bind(principal, RESOURCE_OWNER, topicPrefix, "Group", PREFIX);
 
   }
 
