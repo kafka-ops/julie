a3933d2eabdea3575e2e42e3e19562d05d963b8b
Kirill Kulikov <kikulikov@users.noreply.github.com>
2020-03-30 23:17:55 +0100

    fixed messed up code formatting

--- src/main/java/com/purbon/kafka/topology/BuilderCLI.java
+++ src/main/java/com/purbon/kafka/topology/BuilderCLI.java
@@ -12,99 +12,99 @@ import static java.lang.System.exit;
 
 public class BuilderCLI {
 
-    public static final String TOPOLOGY_OPTION = "topology";
-    public static final String TOPOLOGY_DESC = "Topology config yaml file.";
+  public static final String TOPOLOGY_OPTION = "topology";
+  public static final String TOPOLOGY_DESC = "Topology config yaml file.";
 
-    public static final String BROKERS_OPTION = "brokers";
-    public static final String BROKERS_DESC = "The Apache Kafka server(s) to connect to.";
+  public static final String BROKERS_OPTION = "brokers";
+  public static final String BROKERS_DESC = "The Apache Kafka server(s) to connect to.";
 
-    public static final String ADMIN_CLIENT_CONFIG_OPTION = "clientConfig";
-    public static final String ADMIN_CLIENT_CONFIG_DESC = "The AdminClient configuration file.";
+  public static final String ADMIN_CLIENT_CONFIG_OPTION = "clientConfig";
+  public static final String ADMIN_CLIENT_CONFIG_DESC = "The AdminClient configuration file.";
 
-    public static final String ALLOW_DELETE_OPTION = "allowDelete";
-    public static final String ALLOW_DELETE_DESC = "Permits delete operations for topics and configs.";
+  public static final String ALLOW_DELETE_OPTION = "allowDelete";
+  public static final String ALLOW_DELETE_DESC = "Permits delete operations for topics and configs.";
 
-    public static final String HELP_OPTION = "help";
-    public static final String HELP_DESC = "Prints usage information.";
+  public static final String HELP_OPTION = "help";
+  public static final String HELP_DESC = "Prints usage information.";
 
-    public static Options buildOptions() {
+  public static Options buildOptions() {
 
-        final Option topologyFileOption = Option.builder()
-                .longOpt(TOPOLOGY_OPTION).hasArg().desc(TOPOLOGY_DESC).required().build();
+    final Option topologyFileOption = Option.builder()
+        .longOpt(TOPOLOGY_OPTION).hasArg().desc(TOPOLOGY_DESC).required().build();
 
-        final Option brokersListOption = Option.builder()
-                .longOpt(BROKERS_OPTION).hasArg().desc(BROKERS_DESC).required().build();
+    final Option brokersListOption = Option.builder()
+        .longOpt(BROKERS_OPTION).hasArg().desc(BROKERS_DESC).required().build();
 
-        final Option adminClientConfigFileOption = Option.builder()
-                .longOpt(ADMIN_CLIENT_CONFIG_OPTION).hasArg().desc(ADMIN_CLIENT_CONFIG_DESC).required().build();
+    final Option adminClientConfigFileOption = Option.builder()
+        .longOpt(ADMIN_CLIENT_CONFIG_OPTION).hasArg().desc(ADMIN_CLIENT_CONFIG_DESC).required().build();
 
-        final Option allowDeleteOption = Option.builder()
-                .longOpt(ALLOW_DELETE_OPTION).hasArg(false).desc(ALLOW_DELETE_DESC).required(false).build();
+    final Option allowDeleteOption = Option.builder()
+        .longOpt(ALLOW_DELETE_OPTION).hasArg(false).desc(ALLOW_DELETE_DESC).required(false).build();
 
-        final Option helpOption = Option.builder()
-                .longOpt(HELP_OPTION).hasArg(false).desc(HELP_DESC).required(false).build();
+    final Option helpOption = Option.builder()
+        .longOpt(HELP_OPTION).hasArg(false).desc(HELP_DESC).required(false).build();
 
-        final Options options = new Options();
-        options.addOption(topologyFileOption);
-        options.addOption(brokersListOption);
-        options.addOption(adminClientConfigFileOption);
-        options.addOption(allowDeleteOption);
-        options.addOption(helpOption);
+    final Options options = new Options();
+    options.addOption(topologyFileOption);
+    options.addOption(brokersListOption);
+    options.addOption(adminClientConfigFileOption);
+    options.addOption(allowDeleteOption);
+    options.addOption(helpOption);
 
-        return options;
+    return options;
+  }
+
+  public static void main(String[] args) throws IOException {
+
+    Options options = buildOptions();
+    HelpFormatter formatter = new HelpFormatter();
+    CommandLineParser parser = new DefaultParser();
+    CommandLine cmd = parseArgsOrExit(parser, options, args, formatter);
+
+    if (cmd.hasOption("help")) {
+      formatter.printHelp("kafka-topology-builder", options);
+    } else {
+      String topology = cmd.getOptionValue(TOPOLOGY_OPTION);
+      String brokersList = cmd.getOptionValue(BROKERS_OPTION);
+      boolean allowDelete = cmd.hasOption(ALLOW_DELETE_OPTION);
+      String adminClientConfigFile = cmd.getOptionValue(ADMIN_CLIENT_CONFIG_OPTION);
+
+      Map<String, String> config = new HashMap<>();
+      config.put(BROKERS_OPTION, brokersList);
+      config.put(ALLOW_DELETE_OPTION, String.valueOf(allowDelete));
+      config.put(ADMIN_CLIENT_CONFIG_OPTION, adminClientConfigFile);
+      processTopology(topology, config);
+      System.out.println("Kafka Topology updated");
+    }
+  }
+
+  private static CommandLine parseArgsOrExit(CommandLineParser parser, Options options, String[] args, HelpFormatter formatter) {
+    CommandLine cmd = null;
+    try {
+      cmd = parser.parse(options, args);
+    } catch (ParseException e) {
+      System.out.println("Parsing failed cause of " + e.getMessage());
+      formatter.printHelp("cli", options);
+      exit(1);
+    }
+    return cmd;
+  }
+
+  private static void processTopology(String topologyFile, Map<String, String> config) throws IOException {
+    verifyRequiredParameters(topologyFile, config);
+    KafkaTopologyBuilder builder = new KafkaTopologyBuilder(topologyFile, config);
+    builder.run();
+  }
+
+  private static void verifyRequiredParameters(String topologyFile, Map<String, String> config) throws IOException {
+    if (!Files.exists(Paths.get(topologyFile))) {
+      throw new IOException("Topology file does not exist");
     }
 
-    public static void main(String[] args) throws IOException {
+    String configFilePath = config.get(BuilderCLI.ADMIN_CLIENT_CONFIG_OPTION);
 
-        Options options = buildOptions();
-        HelpFormatter formatter = new HelpFormatter();
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = parseArgsOrExit(parser, options, args, formatter);
-
-        if (cmd.hasOption("help")) {
-            formatter.printHelp("kafka-topology-builder", options);
-        } else {
-            String topology = cmd.getOptionValue(TOPOLOGY_OPTION);
-            String brokersList = cmd.getOptionValue(BROKERS_OPTION);
-            boolean allowDelete = cmd.hasOption(ALLOW_DELETE_OPTION);
-            String adminClientConfigFile = cmd.getOptionValue(ADMIN_CLIENT_CONFIG_OPTION);
-
-            Map<String, String> config = new HashMap<>();
-            config.put(BROKERS_OPTION, brokersList);
-            config.put(ALLOW_DELETE_OPTION, String.valueOf(allowDelete));
-            config.put(ADMIN_CLIENT_CONFIG_OPTION, adminClientConfigFile);
-            processTopology(topology, config);
-            System.out.println("Kafka Topology updated");
-        }
+    if (!Files.exists(Paths.get(configFilePath))) {
+      throw new IOException("AdminClient config file does not exist");
     }
-
-    private static CommandLine parseArgsOrExit(CommandLineParser parser, Options options, String[] args, HelpFormatter formatter) {
-        CommandLine cmd = null;
-        try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            System.out.println("Parsing failed cause of " + e.getMessage());
-            formatter.printHelp("cli", options);
-            exit(1);
-        }
-        return cmd;
-    }
-
-    private static void processTopology(String topologyFile, Map<String, String> config) throws IOException {
-        verifyRequiredParameters(topologyFile, config);
-        KafkaTopologyBuilder builder = new KafkaTopologyBuilder(topologyFile, config);
-        builder.run();
-    }
-
-    private static void verifyRequiredParameters(String topologyFile, Map<String, String> config)  throws IOException {
-        if (!Files.exists(Paths.get(topologyFile))) {
-            throw new IOException("Topology file does not exist");
-        }
-
-        String configFilePath = config.get(BuilderCLI.ADMIN_CLIENT_CONFIG_OPTION);
-
-        if (!Files.exists(Paths.get(configFilePath))) {
-            throw new IOException("AdminClient config file does not exist");
-        }
-    }
+  }
 }
--- src/main/java/com/purbon/kafka/topology/ClusterState.java
+++ src/main/java/com/purbon/kafka/topology/ClusterState.java
@@ -11,44 +11,44 @@ import java.util.function.Consumer;
 
 public class ClusterState {
 
-    private static final String STATE_TYPE = "acls";
+  private static final String STATE_TYPE = "acls";
 
-    private final StateProcessor stateProcessor;
-    private List<TopologyAclBinding> bindings;
+  private final StateProcessor stateProcessor;
+  private List<TopologyAclBinding> bindings;
 
-    public ClusterState() {
-        this(new FileSateProcessor());
-    }
+  public ClusterState() {
+    this(new FileSateProcessor());
+  }
 
-    public ClusterState(StateProcessor stateProcessor) {
-        this.stateProcessor = stateProcessor;
-        this.bindings = new ArrayList<>();
-    }
+  public ClusterState(StateProcessor stateProcessor) {
+    this.stateProcessor = stateProcessor;
+    this.bindings = new ArrayList<>();
+  }
 
-    public void update(List<TopologyAclBinding> bindings) {
-        this.bindings.addAll(bindings);
-    }
+  public void update(List<TopologyAclBinding> bindings) {
+    this.bindings.addAll(bindings);
+  }
 
-    public void forEachBinding(Consumer<? super TopologyAclBinding> action) {
-        bindings.forEach(action);
-    }
+  public void forEachBinding(Consumer<? super TopologyAclBinding> action) {
+    bindings.forEach(action);
+  }
 
-    public void flushAndClose() {
-        stateProcessor.saveType(STATE_TYPE);
-        stateProcessor.saveBindings(bindings);
-        stateProcessor.close();
-    }
+  public void flushAndClose() {
+    stateProcessor.saveType(STATE_TYPE);
+    stateProcessor.saveBindings(bindings);
+    stateProcessor.close();
+  }
 
-    public void load() throws IOException {
-        bindings = stateProcessor.load();
-    }
+  public void load() throws IOException {
+    bindings = stateProcessor.load();
+  }
 
-    public void reset() {
-        bindings.clear();
-        stateProcessor.createOrOpen();
-    }
+  public void reset() {
+    bindings.clear();
+    stateProcessor.createOrOpen();
+  }
 
-    public int size() {
-        return bindings.size();
-    }
+  public int size() {
+    return bindings.size();
+  }
 }
