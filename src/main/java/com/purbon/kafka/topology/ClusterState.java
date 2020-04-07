24d9e3ba6e333b9b98019aed3bc74ed605887c5a
Kirill Kulikov <2751459+kikulikov@users.noreply.github.com>
2020-04-07 08:49:22 +0100

    Basic code clean up (#17)
    * pom.xml to compile with java 13 & code clean up
    
    * build fat jar with package
    
    * cli options fixed as incorrect builder methods used
    
    * fixed messed up code formatting
    
    * code updated on code review
    
    Co-authored-by: Kirill Kulikov <kikulikov@users.noreply.github.com>

--- src/main/java/com/purbon/kafka/topology/ClusterState.java
+++ src/main/java/com/purbon/kafka/topology/ClusterState.java
@@ -2,43 +2,28 @@ package com.purbon.kafka.topology;
 
 import com.purbon.kafka.topology.clusterstate.FileSateProcessor;
 import com.purbon.kafka.topology.clusterstate.StateProcessor;
-import com.purbon.kafka.topology.model.Topology;
 import com.purbon.kafka.topology.roles.TopologyAclBinding;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
+
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.Writer;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import org.apache.kafka.common.acl.AclBinding;
 
 public class ClusterState {
 
-  private final StateProcessor stateProcessor;
-  private String type;
-  private List<TopologyAclBinding> bindings;
+  private static final String STORE_TYPE = "acls";
 
+  private final StateProcessor stateProcessor;
+  private List<TopologyAclBinding> bindings;
 
   public ClusterState() {
     this(new FileSateProcessor());
   }
 
   public ClusterState(StateProcessor stateProcessor) {
-    this.type = "acls";
     this.stateProcessor = stateProcessor;
     this.bindings = new ArrayList<>();
   }
-
 
   public void update(List<TopologyAclBinding> bindings) {
     this.bindings.addAll(bindings);
@@ -49,13 +34,13 @@ public class ClusterState {
   }
 
   public void flushAndClose() {
-    stateProcessor.saveType(type);
+    stateProcessor.saveType(STORE_TYPE);
     stateProcessor.saveBindings(bindings);
     stateProcessor.close();
   }
 
   public void load() throws IOException {
-   bindings = stateProcessor.load();
+    bindings = stateProcessor.load();
   }
 
   public void reset() {
