be93ca52a57720b9abf8745e5d795ccaed2d08b1
Pere Urbon <pere@confluent.io>
2020-07-04 18:20:01 +0200

    use the group name as well for the basic acls configuration

--- src/main/java/com/purbon/kafka/topology/TopologyBuilderAdminClient.java
+++ src/main/java/com/purbon/kafka/topology/TopologyBuilderAdminClient.java
@@ -355,7 +355,7 @@ public class TopologyBuilderAdminClient {
         new AccessControlEntry(principal, "*", AclOperation.CREATE, AclPermissionType.ALLOW);
     acls.add(new AclBinding(resourcePattern, entry));
 
-    resourcePattern = new ResourcePattern(ResourceType.GROUP, "*", PatternType.LITERAL);
+    resourcePattern = new ResourcePattern(ResourceType.GROUP, connector.getGroup(), PatternType.LITERAL);
     entry = new AccessControlEntry(principal, "*", AclOperation.READ, AclPermissionType.ALLOW);
     acls.add(new AclBinding(resourcePattern, entry));
 
