baeaf95a5fdfdc7a82b1a36ec2d2a0c61eb91d72
Pere Urbon-Bayes <pere.urbon@gmail.com>
2020-07-23 09:04:56 +0200

    update demo script to include retention_ms

--- server-api/docker/demo/bootstrap.sh
+++ server-api/docker/demo/bootstrap.sh
@@ -3,7 +3,7 @@
 curl -X POST http://localhost:8080/topologies/foo
 curl -X POST http://localhost:8080/topologies/foo/projects/p1
 
-curl -H "Content-Type: application/json" -d '{"num_partitions":"1", "replication_factor":"1"}' \
+curl -H "Content-Type: application/json" -d '{"num_partitions":"1", "replication_factor":"1", "retention_ms": "10"}' \
      -X POST http://localhost:8080/topologies/foo/projects/p1/topics/t1
 curl -H "Content-Type: application/json" -d '{"num_partitions":"1", "replication_factor":"1"}' \
      -X POST http://localhost:8080/topologies/foo/projects/p1/topics/t2
