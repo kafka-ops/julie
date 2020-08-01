ad1810afda71c7b008c77a7c2a1b3e9004a78205
Pere Urbon <pere@confluent.io>
2020-08-01 17:31:48 +0200

    ammend the use of the shaded generated jar insted of the assembly one, as well add a manifest in the generated jar specifying the main class

--- release/docker/build.sh
+++ release/docker/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-cp  ../../target/kafka-topology-builder-jar-with-dependencies.jar .
+cp  ../../target/kafka-topology-builder.jar .
 cp ../../src/main/scripts/kafka-topology-builder.sh .
 
 docker build  -t purbon/kafka-topology-builder .
 
-rm kafka-topology-builder-jar-with-dependencies.jar
+rm kafka-topology-builder.jar
 rm kafka-topology-builder.sh
