68c03e8aabf04edbd37b5db8baacc13b8368616e
Pere Urbon-Bayes <pere.urbon@gmail.com>
2020-08-18 16:34:14 +0200

    fix typo in the sh script that broke the release artifacts, updated latest from docker already

--- src/main/scripts/kafka-topology-builder.sh
+++ src/main/scripts/kafka-topology-builder.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 KAFKA_TOPOLOGY_BUILDER_HOME=/usr/local/kafka-topology-builder
-KTB_JAR=$KAFKA_TOPOLOGY_BUILDER_HOME/bin/kafka-topology-builder-jar.jar
+KTB_JAR=$KAFKA_TOPOLOGY_BUILDER_HOME/bin/kafka-topology-builder.jar
 JAVA_PATH=java
 
 $JAVA_PATH -jar $KTB_JAR "$@"
