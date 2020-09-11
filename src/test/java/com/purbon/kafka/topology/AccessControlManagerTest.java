3e35d634cd631018dff42b8f0daea76d8a9a1bf5
Pere Urbon-Bayes <pere.urbon@gmail.com>
2020-09-11 13:16:03 +0200

    add test for acls delete

--- src/test/java/com/purbon/kafka/topology/AccessControlManagerTest.java
+++ src/test/java/com/purbon/kafka/topology/AccessControlManagerTest.java
@@ -1,5 +1,6 @@
 package com.purbon.kafka.topology;
 
+import static java.util.Arrays.asList;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
@@ -9,6 +10,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.purbon.kafka.topology.actions.Action;
+import com.purbon.kafka.topology.adminclient.AclBuilder;
 import com.purbon.kafka.topology.model.Component;
 import com.purbon.kafka.topology.model.Impl.ProjectImpl;
 import com.purbon.kafka.topology.model.Impl.TopicImpl;
@@ -33,12 +35,18 @@ import com.purbon.kafka.topology.roles.TopologyAclBinding;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import org.apache.kafka.common.acl.AclBinding;
+import org.apache.kafka.common.acl.AclOperation;
+import org.apache.kafka.common.acl.AclPermissionType;
+import org.apache.kafka.common.resource.PatternType;
+import org.apache.kafka.common.resource.ResourceType;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -80,7 +88,7 @@ public class AccessControlManagerTest {
     Topology topology = new TopologyImpl();
     topology.addProject(project);
 
-    List<Consumer> users = Arrays.asList(new Consumer("User:app1"));
+    List<Consumer> users = asList(new Consumer("User:app1"));
 
     doReturn(new ArrayList<TopologyAclBinding>())
         .when(aclsProvider)
@@ -103,7 +111,7 @@ public class AccessControlManagerTest {
     Topology topology = new TopologyImpl();
     topology.addProject(project);
 
-    List<String> users = Arrays.asList(new String[] {"User:app1"});
+    List<String> users = asList(new String[] {"User:app1"});
 
     doReturn(new ArrayList<TopologyAclBinding>())
         .when(aclsProvider)
@@ -120,8 +128,8 @@ public class AccessControlManagerTest {
     KStream app = new KStream();
     app.setPrincipal("User:App0");
     HashMap<String, List<String>> topics = new HashMap<>();
-    topics.put(KStream.READ_TOPICS, Arrays.asList("topicA", "topicB"));
-    topics.put(KStream.WRITE_TOPICS, Arrays.asList("topicC", "topicD"));
+    topics.put(KStream.READ_TOPICS, asList("topicA", "topicB"));
+    topics.put(KStream.WRITE_TOPICS, asList("topicC", "topicD"));
     app.setTopics(topics);
     project.setStreams(Collections.singletonList(app));
 
@@ -270,10 +278,10 @@ public class AccessControlManagerTest {
     Connector connector1 = new Connector();
     connector1.setPrincipal("User:Connect1");
     HashMap<String, List<String>> topics = new HashMap<>();
-    topics.put(Connector.READ_TOPICS, Arrays.asList("topicA", "topicB"));
+    topics.put(Connector.READ_TOPICS, asList("topicA", "topicB"));
     connector1.setTopics(topics);
 
-    project.setConnectors(Arrays.asList(connector1));
+    project.setConnectors(asList(connector1));
 
     Topology topology = new TopologyImpl();
     topology.addProject(project);
@@ -305,7 +313,7 @@ public class AccessControlManagerTest {
     Topology topology = new TopologyImpl();
     topology.addProject(project);
 
-    List<Consumer> users = Arrays.asList(new Consumer("User:app1"));
+    List<Consumer> users = asList(new Consumer("User:app1"));
 
     doReturn(new ArrayList<TopologyAclBinding>())
         .when(aclsProvider)
@@ -313,5 +321,97 @@ public class AccessControlManagerTest {
     accessControlManager.sync(topology);
 
     verify(mockPrintStream, times(2)).println(any(Action.class));
+  }
+
+  @Test
+  public void testAclDeleteLogic() throws IOException {
+
+    accessControlManager = new AccessControlManager(aclsProvider);
+
+    List<Consumer> consumers = new ArrayList<>();
+    consumers.add(new Consumer("User:app1"));
+    consumers.add(new Consumer("User:app2"));
+
+    Topic topicA = new TopicImpl("topicA");
+
+    Topology topology = buildTopology(consumers, asList(topicA));
+
+    List<TopologyAclBinding> bindings = returnAclsForConsumers(consumers, topicA.getName());
+    doReturn(bindings).when(aclsProvider).setAclsForConsumers(any(), eq(topicA.toString()));
+
+    accessControlManager.sync(topology);
+
+    verify(aclsProvider, times(1)).setAclsForConsumers(asList(consumers.get(0)), topicA.toString());
+    verify(aclsProvider, times(1)).setAclsForConsumers(asList(consumers.get(1)), topicA.toString());
+
+    consumers = new ArrayList<>();
+    consumers.add(new Consumer("User:app1"));
+
+    bindings = returnAclsForConsumers(consumers, topicA.getName());
+    doReturn(bindings).when(aclsProvider).setAclsForConsumers(eq(consumers), eq(topicA.toString()));
+
+    Topology newTopology = buildTopology(consumers, asList(topicA));
+
+    accessControlManager.sync(newTopology);
+
+    List<TopologyAclBinding> bindingsToDelete =
+        returnAclsForConsumers(asList(new Consumer("User:app2")), topicA.getName());
+
+    verify(aclsProvider, times(1)).clearAcls(new HashSet<>(bindingsToDelete));
+  }
+
+  private Topology buildTopology(List<Consumer> consumers, List<Topic> topics) {
+
+    Project project = new ProjectImpl();
+    project.setConsumers(consumers);
+
+    for (Topic topic : topics) {
+      project.addTopic(topic);
+    }
+
+    Topology topology = new TopologyImpl();
+    topology.addProject(project);
+
+    return topology;
+  }
+
+  private List<TopologyAclBinding> returnAclsForConsumers(List<Consumer> consumers, String topic) {
+
+    List<AclBinding> acls = new ArrayList<>();
+
+    for (Consumer consumer : consumers) {
+      acls.add(
+          buildTopicLevelAcl(
+              consumer.getPrincipal(), topic, PatternType.LITERAL, AclOperation.DESCRIBE));
+      acls.add(
+          buildTopicLevelAcl(
+              consumer.getPrincipal(), topic, PatternType.LITERAL, AclOperation.READ));
+      acls.add(
+          buildGroupLevelAcl(
+              consumer.getPrincipal(),
+              consumer.groupString(),
+              consumer.groupString().equals("*") ? PatternType.PREFIXED : PatternType.LITERAL,
+              AclOperation.READ));
+    }
+
+    return acls.stream()
+        .map(aclBinding -> new TopologyAclBinding(aclBinding))
+        .collect(Collectors.toList());
+  }
+
+  private AclBinding buildTopicLevelAcl(
+      String principal, String topic, PatternType patternType, AclOperation op) {
+    return new AclBuilder(principal)
+        .addResource(ResourceType.TOPIC, topic, patternType)
+        .addControlEntry("*", op, AclPermissionType.ALLOW)
+        .build();
+  }
+
+  private AclBinding buildGroupLevelAcl(
+      String principal, String group, PatternType patternType, AclOperation op) {
+    return new AclBuilder(principal)
+        .addResource(ResourceType.GROUP, group, patternType)
+        .addControlEntry("*", op, AclPermissionType.ALLOW)
+        .build();
   }
 }
