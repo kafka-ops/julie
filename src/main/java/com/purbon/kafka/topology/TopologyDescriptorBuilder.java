6dd1e19daf8426a72deaec9877b78a17bcc085ab
Pere Urbón <purbon@users.noreply.github.com>
2020-10-24 10:32:45 +0200

    Enable JSON and YAML as Topology descriptor file formats. (#108)
    * add option to handle as well json files
    
    * add code to handle descriptor files as well in json format
    
    * revert the xml support experiment
    
    * clean up access control class
    
    * update quick note in the docs
    
    * a bit more bits in the docs
    
    * example update

--- src/main/java/com/purbon/kafka/topology/TopologyDescriptorBuilder.java
+++ src/main/java/com/purbon/kafka/topology/TopologyDescriptorBuilder.java
@@ -9,8 +9,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class TopologyDescriptorBuilder {
+
+  private static final Logger LOGGER = LogManager.getLogger(TopologyDescriptorBuilder.class);
 
   public static Topology build(String fileOrDir) throws IOException {
     return build(fileOrDir, new TopologyBuilderConfig());
@@ -44,7 +48,7 @@ public class TopologyDescriptorBuilder {
                 try {
                   return parser.deserialise(path.toFile());
                 } catch (IOException e) {
-                  e.printStackTrace();
+                  LOGGER.error(e);
                   return new TopologyImpl();
                 }
               })
