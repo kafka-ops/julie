7f72c24fb162339f02684ac76343e3977ff830da
Pere Urbón <purbon@users.noreply.github.com>
2020-11-16 15:50:03 +0100

    make usage of key schemas optional, but throw an exception if key is present but not value schemas (#128)

--- src/main/java/com/purbon/kafka/topology/model/TopicSchemas.java
+++ src/main/java/com/purbon/kafka/topology/model/TopicSchemas.java
@@ -1,24 +1,37 @@
 package com.purbon.kafka.topology.model;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.util.Optional;
 
 @JsonNaming(PropertyNamingStrategy.LowerDotCaseStrategy.class)
 public class TopicSchemas {
 
-  private String keySchemaFile;
-  private String valueSchemaFile;
+  private Optional<String> keySchemaFile;
+  private Optional<String> valueSchemaFile;
 
-  public TopicSchemas(String keySchemaFile, String valueSchemaFile) {
-    this.keySchemaFile = keySchemaFile;
-    this.valueSchemaFile = valueSchemaFile;
+  public TopicSchemas() {
+    this(Optional.empty(), Optional.empty());
   }
 
-  public String getKeySchemaFile() {
+  public TopicSchemas(Optional<JsonNode> keyJsonNode, Optional<JsonNode> valueJsonNode) {
+    this.keySchemaFile = Optional.empty();
+    this.valueSchemaFile = Optional.empty();
+    keyJsonNode.ifPresent(node -> keySchemaFile = Optional.of(node.asText()));
+    valueJsonNode.ifPresent(node -> valueSchemaFile = Optional.of(node.asText()));
+  }
+
+  public TopicSchemas(String keySchemaFile, String valueSchemaFile) {
+    this.keySchemaFile = Optional.of(keySchemaFile);
+    this.valueSchemaFile = Optional.of(valueSchemaFile);
+  }
+
+  public Optional<String> getKeySchemaFile() {
     return keySchemaFile;
   }
 
-  public String getValueSchemaFile() {
+  public Optional<String> getValueSchemaFile() {
     return valueSchemaFile;
   }
 }
