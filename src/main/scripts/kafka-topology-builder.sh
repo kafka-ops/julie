40311e41df1afb0f02727211a5eeae3fe7dc36cc
Eduardo Solis <eduardo.solis@udemy.com>
2020-12-22 16:30:50 -0600

    Adding ability to pass jvm options via environment for docker

--- src/main/scripts/kafka-topology-builder.sh
+++ src/main/scripts/kafka-topology-builder.sh
@@ -4,4 +4,4 @@ KAFKA_TOPOLOGY_BUILDER_HOME=/usr/local/kafka-topology-builder
 KTB_JAR=$KAFKA_TOPOLOGY_BUILDER_HOME/bin/kafka-topology-builder.jar
 JAVA_PATH=java
 
-$JAVA_PATH -jar $KTB_JAR "$@"
+$JAVA_PATH $JVM_OPTIONS -jar $KTB_JAR "$@"
