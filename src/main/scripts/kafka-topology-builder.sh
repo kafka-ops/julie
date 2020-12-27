4196f900479d5655d50c9f008831910c8f6e6818
Eduardo Solis <eduardo.solis@udemy.com>
2020-12-27 03:23:54 -0600

    Adding ability to pass KTB options for the jvm or the KTB itself via an environment variable (#177)
    * Adding ability to pass jvm options via environment for docker
    
    * name the options variable  and introduce a default value if not previously defined
    
    Co-authored-by: Pere Urbon <pere@confluent.io>

--- src/main/scripts/kafka-topology-builder.sh
+++ src/main/scripts/kafka-topology-builder.sh
@@ -4,4 +4,8 @@ KAFKA_TOPOLOGY_BUILDER_HOME=/usr/local/kafka-topology-builder
 KTB_JAR=$KAFKA_TOPOLOGY_BUILDER_HOME/bin/kafka-topology-builder.jar
 JAVA_PATH=java
 
-$JAVA_PATH -jar $KTB_JAR "$@"
+if [ -z "$KTB_OPTIONS" ]; then
+  KTB_OPTIONS=""
+fi
+
+$JAVA_PATH $KTB_OPTIONS -jar $KTB_JAR "$@"
