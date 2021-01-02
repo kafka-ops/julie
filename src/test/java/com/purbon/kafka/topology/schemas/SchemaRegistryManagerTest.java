0fee6914df1146bfdffc22e8d59f20bcc1a56d7d
Pere Urbon-Bayes <pere.urbon@gmail.com>
2021-01-02 09:33:47 +0100

    add basic test

--- src/test/java/com/purbon/kafka/topology/schemas/SchemaRegistryManagerTest.java
+++ src/test/java/com/purbon/kafka/topology/schemas/SchemaRegistryManagerTest.java
@@ -6,6 +6,9 @@ import com.purbon.kafka.topology.schemas.SchemaRegistryManager.SchemaRegistryMan
 import io.confluent.kafka.schemaregistry.avro.AvroSchema;
 import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.junit.Before;
@@ -48,6 +51,20 @@ public class SchemaRegistryManagerTest {
 
     assertThat(client.getAllSubjects()).hasSize(1).containsExactly(subjectName);
     assertThat(client.getAllVersions(subjectName)).hasSize(1).containsExactly(1);
+  }
+
+  @Test
+  public void shouldRegisterTheSchemawithCompatibility()
+      throws IOException, RestClientException, URISyntaxException {
+    Path schemaFilePath =
+        Paths.get(getClass().getClassLoader().getResource("schemas/bar-value.avsc").toURI());
+
+    final int subjectId = manager.register(subjectName, schemaFilePath, AvroSchema.TYPE);
+    assertThat(subjectId).isEqualTo(1);
+
+    String compLevel = manager.setCompatibility(subjectName, "FORWARD");
+    assertThat(compLevel).isEqualTo("FORWARD");
+    assertThat(client.getCompatibility(subjectName)).isEqualTo("FORWARD");
   }
 
   @Test(expected = SchemaRegistryManagerException.class)
