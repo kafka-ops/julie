2d11b3ef8ec6afe8b16dfb74d9e925289424d282
Pere Urbon-Bayes <pere.urbon@gmail.com>
2021-04-01 17:27:41 +0200

    hookup the configuration flow within all the backends

--- src/main/java/com/purbon/kafka/topology/backend/Backend.java
+++ src/main/java/com/purbon/kafka/topology/backend/Backend.java
@@ -1,9 +1,14 @@
 package com.purbon.kafka.topology.backend;
 
 import com.purbon.kafka.topology.BackendController;
+import com.purbon.kafka.topology.Configuration;
 import java.io.IOException;
 
 public interface Backend {
+
+  default void configure(Configuration config) {
+    // empty if not implemented
+  }
 
   void createOrOpen();
 
