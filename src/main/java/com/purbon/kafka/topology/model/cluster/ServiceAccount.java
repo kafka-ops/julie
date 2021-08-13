b31df6ea20332876a7817bb6ec849f40042ba8e2
Michael Van Brummen <michaelvanbrummen@gmail.com>
2021-08-13 14:07:31 +1000

    Added resourceId to ServiceAccount.java to support ccloud v1.28.0+

--- src/main/java/com/purbon/kafka/topology/model/cluster/ServiceAccount.java
+++ src/main/java/com/purbon/kafka/topology/model/cluster/ServiceAccount.java
@@ -7,15 +7,21 @@ public class ServiceAccount {
   private int id;
   private String name;
   private String description;
+  private String resourceId;
 
   public ServiceAccount() {
-    this(-1, "", "");
+    this(-1, "", "", "");
   }
 
   public ServiceAccount(int id, String name, String description) {
+    this(id, name, description, "");
+  }
+
+  public ServiceAccount(int id, String name, String description, String resourceId) {
     this.id = id;
     this.name = name;
     this.description = description;
+    this.resourceId = resourceId;
   }
 
   public int getId() {
@@ -30,11 +36,16 @@ public class ServiceAccount {
     return description;
   }
 
+  public String getResourceId() {
+    return resourceId;
+  }
+
   @Override
   public String toString() {
     final StringBuffer sb = new StringBuffer("ServiceAccount{");
     sb.append("id=").append(id);
     sb.append(", name='").append(name).append('\'');
+    sb.append(", resourceId='").append(resourceId).append('\'');
     sb.append('}');
     return sb.toString();
   }
@@ -53,6 +64,6 @@ public class ServiceAccount {
 
   @Override
   public int hashCode() {
-    return Objects.hash(getId(), getName(), getDescription());
+    return Objects.hash(getId(), getName(), getDescription(), getResourceId());
   }
 }
