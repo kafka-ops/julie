5c9b99805c8ad550ec57a1b28b59f288ddfe4732
Pere Urbon-Bayes <pere.urbon@gmail.com>
2021-08-18 12:35:23 +0200

    add a kerbereized kafka deployment as docker-compose for test and examples

---
+++ example/scripts/run-kerberos-app.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+kinit -k -t /var/lib/secret/kafka-admin.key admin/for-kafka
+
+julie-ops-cli.sh  \
+    --brokers kafka:9093 \
+    --topology /app/example/descriptor-only-topics.yaml \
+    --clientConfig /app/example/topology-builder-kerberos.properties
