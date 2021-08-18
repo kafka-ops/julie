75be3afbe4b579622eb6a528b5a28fc3e751ea7f
Pere Urbon-Bayes <pere.urbon@gmail.com>
2021-08-18 12:39:33 +0200

    add an example for running in kerberos

--- example/scripts/run-kerberos-app.sh
+++ example/scripts/run-kerberos-app.sh
@@ -5,4 +5,5 @@ kinit -k -t /var/lib/secret/kafka-admin.key admin/for-kafka
 julie-ops-cli.sh  \
     --brokers kafka:9093 \
     --topology /app/example/descriptor-only-topics.yaml \
+    --allowDelete \
     --clientConfig /app/example/topology-builder-kerberos.properties
