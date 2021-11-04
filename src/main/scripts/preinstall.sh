5015955b45d6de56e5f39b8d3e4b8a11453c3bb6
Pere Urbón <purbon@users.noreply.github.com>
2021-11-04 10:32:27 +0100

    Add pre install hook verification for requirement java  (#371)
    * add verification of java version to be at least 11 during the pre install hook for rpm
    
    * ammend return code capture
    
    * Update src/main/scripts/preinstall.sh
    
    Co-authored-by: Fobhep <hopfenmueller@atix.de>
    
    * Update src/main/scripts/preinstall.sh
    
    Co-authored-by: Fobhep <hopfenmueller@atix.de>
    
    Co-authored-by: Fobhep <hopfenmueller@atix.de>

--- src/main/scripts/preinstall.sh
+++ src/main/scripts/preinstall.sh
@@ -1,8 +1,32 @@
 #!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+function default_user_creation_if_required {
+  if id "julie-kafka" &>/dev/null;
+  then
+    echo "User julie-kafka already exist"
+  else
+    adduser -M -r julie-kafka
+  fi
+}
 
-if id "julie-kafka" &>/dev/null;
-then
-  echo "User julie-kafka already exist"
-else
-   adduser -M -r julie-kafka
-fi
+function verify_java_version_min_11 {
+    current_version=$(java -version 2>&1 | tr "\n" " " | cut -d " " -f 3  | tr -d '"' | cut -d "." -f 1)
+    if (( current_version < 11 ))
+    then
+      return 1;
+    fi
+    return 0
+}
+
+
+default_user_creation_if_required
+
+# Verify the existence of java command, if not there verification should exit already
+type -P java &> /dev/null || { echo "java not found"; exit 1; }
+
+verify_java_version_min_11
+exit_code=$?
+
+exit $exit_code
