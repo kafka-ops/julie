f9afa5104ce449ee4759d505173dff996b8e3333
Pere Urbón <purbon@users.noreply.github.com>
2021-11-12 17:38:46 +0100

    fix/handling of connectors only without defining acls (#397)

--- src/main/java/com/purbon/kafka/topology/serdes/TopologyCustomDeserializer.java
+++ src/main/java/com/purbon/kafka/topology/serdes/TopologyCustomDeserializer.java
@@ -341,6 +341,10 @@ public class TopologyCustomDeserializer extends StdDeserializer<Topology> {
         }
       }
     }
+    // bloody hack that needs to be cleanned. This is to support not having ACLS defined properly and only connectors.
+    if (connectors.size() == 1 && connectors.get(0) == null) {
+      connectors = new ArrayList<>();
+    }
     return Optional.of(new PlatformSystem(connectors, new KConnectArtefacts(artefacts)));
   }
 
