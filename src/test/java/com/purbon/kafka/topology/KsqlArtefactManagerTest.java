ed61aa532204dadec8210a093dc8b7a42c2ef05c
Pere Urbón <purbon@users.noreply.github.com>
2021-11-17 11:37:05 +0100

    ammend order for delete in ksql to be reverse from creation (#400)

--- src/test/java/com/purbon/kafka/topology/KsqlArtefactManagerTest.java
+++ src/test/java/com/purbon/kafka/topology/KsqlArtefactManagerTest.java
@@ -10,6 +10,7 @@ import com.purbon.kafka.topology.model.PlanMap;
 import com.purbon.kafka.topology.model.Topology;
 import com.purbon.kafka.topology.serdes.TopologySerdes;
 import com.purbon.kafka.topology.utils.TestUtils;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -69,6 +70,31 @@ public class KsqlArtefactManagerTest {
         assertThat(artefact).isEqualTo(ksqlArtefacts.getTables().get(i - 2));
       }
       i++;
+    }
+  }
+
+  @Test
+  public void testArtefactsForDeletionOrder() {
+    String topologyFileName = "/descriptor-ksql-multiple.yaml";
+    String topologyFilePath = TestUtils.getResourceFilename(topologyFileName);
+
+    Topology topology = parser.deserialise(TestUtils.getResourceFile(topologyFileName));
+
+    KSqlArtefactManager m = new KSqlArtefactManager(client, config, topologyFilePath);
+
+    var artefacts = m.parseNewArtefacts(topology);
+
+    System.out.println(artefacts);
+
+    var toDelete = m.findArtefactsToBeDeleted(artefacts, Collections.emptySet());
+    System.out.println(toDelete);
+
+    assertThat(toDelete).hasSize(4);
+
+    int j = toDelete.size() - 1;
+    for (Artefact artefact : artefacts) {
+      assertThat(artefact).isEqualTo(toDelete.get(j));
+      j--;
     }
   }
 }
