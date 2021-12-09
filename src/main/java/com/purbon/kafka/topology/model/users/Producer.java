ded8a1169333f5c406822c22db825bb2b4042ad1
Pere Urbon-Bayes <pere.urbon@gmail.com>
2021-12-09 15:07:27 +0100

    exclude from jackson serdes

--- src/main/java/com/purbon/kafka/topology/model/users/Producer.java
+++ src/main/java/com/purbon/kafka/topology/model/users/Producer.java
@@ -1,5 +1,6 @@
 package com.purbon.kafka.topology.model.users;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.purbon.kafka.topology.model.User;
 import java.util.Objects;
 import java.util.Optional;
@@ -58,10 +59,12 @@ public class Producer extends User {
     return Objects.hash(getPrincipal());
   }
 
+  @JsonIgnore
   public boolean hasTransactionId() {
     return transactionId.isPresent();
   }
 
+  @JsonIgnore
   public boolean isIdempotent() {
     return idempotence.isPresent();
   }
