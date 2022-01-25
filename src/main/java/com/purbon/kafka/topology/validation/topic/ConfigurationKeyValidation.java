14da3665dee3f6a6c89ceb1bac875ed0e5762fe8
Pere Urbon-Bayes <pere.urbon@gmail.com>
2022-01-25 17:10:50 +0100

    fix config passing for topology validator for regular expressions

--- src/main/java/com/purbon/kafka/topology/validation/topic/ConfigurationKeyValidation.java
+++ src/main/java/com/purbon/kafka/topology/validation/topic/ConfigurationKeyValidation.java
@@ -1,11 +1,16 @@
 package com.purbon.kafka.topology.validation.topic;
 
+import com.purbon.kafka.topology.Configuration;
 import com.purbon.kafka.topology.exceptions.ValidationException;
 import com.purbon.kafka.topology.model.Topic;
 import com.purbon.kafka.topology.validation.TopicValidation;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Map;
+
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.common.config.TopicConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,22 +18,26 @@ import org.apache.logging.log4j.Logger;
 /**
  * This validation checks that all topic configs are valid ones according to the TopicConfig class.
  */
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class ConfigurationKeyValidation implements TopicValidation {
 
   private static final Logger LOGGER = LogManager.getLogger(ConfigurationKeyValidation.class);
 
+  @NonNull private Configuration config;
+
   @Override
   public void valid(Topic topic) throws ValidationException {
     Field[] fields = TopicConfig.class.getDeclaredFields();
-    TopicConfig config = new TopicConfig();
-    Map<String, String> topicConfig = getTopicConfig(topic);
-    for (Map.Entry<String, String> entry : topicConfig.entrySet()) {
+    TopicConfig topicConfig = new TopicConfig();
+    Map<String, String> topicConfigMap = getTopicConfig(topic);
+    for (Map.Entry<String, String> entry : topicConfigMap.entrySet()) {
       boolean match =
           Arrays.stream(fields)
               .anyMatch(
                   field -> {
                     try {
-                      return ((String) field.get(config)).contains(entry.getKey());
+                      return ((String) field.get(topicConfig)).contains(entry.getKey());
                     } catch (IllegalAccessException e) {
                       LOGGER.error(e);
                       return false;
--- src/main/java/com/purbon/kafka/topology/validation/topic/ReplicationFactorValidation.java
+++ src/main/java/com/purbon/kafka/topology/validation/topic/ReplicationFactorValidation.java
@@ -1,10 +1,19 @@
 package com.purbon.kafka.topology.validation.topic;
 
+import com.purbon.kafka.topology.Configuration;
 import com.purbon.kafka.topology.exceptions.ValidationException;
 import com.purbon.kafka.topology.model.Topic;
 import com.purbon.kafka.topology.validation.TopicValidation;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class ReplicationFactorValidation implements TopicValidation {
+
+  @NonNull
+  private Configuration config;
 
   @Override
   public void valid(Topic topic) throws ValidationException {
