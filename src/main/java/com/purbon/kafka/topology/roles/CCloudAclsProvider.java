181ef29439dde49763db06ee0cbcd2d722d04ef0
Pere Urbón <purbon@users.noreply.github.com>
2022-04-02 10:56:18 +0200

    Refactor and add tests related to Confluent Cloud service account translation feature (#468)
    * clean up and run tests for the service account translation service for confluent cloud
    
    * add simple error test

--- src/main/java/com/purbon/kafka/topology/roles/CCloudAclsProvider.java
+++ src/main/java/com/purbon/kafka/topology/roles/CCloudAclsProvider.java
@@ -4,14 +4,13 @@ import com.purbon.kafka.topology.AccessControlProvider;
 import com.purbon.kafka.topology.Configuration;
 import com.purbon.kafka.topology.api.adminclient.TopologyBuilderAdminClient;
 import com.purbon.kafka.topology.api.ccloud.CCloudApi;
-import com.purbon.kafka.topology.model.users.ConfluentCloudPrincipal;
+import com.purbon.kafka.topology.utils.CCloudUtils;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -19,12 +18,11 @@ import org.apache.logging.log4j.Logger;
 public class CCloudAclsProvider extends SimpleAclsProvider implements AccessControlProvider {
 
   private static final Logger LOGGER = LogManager.getLogger(CCloudAclsProvider.class);
-  private static final long SERVICE_ACCOUNT_NOT_FOUND = -1L;
 
   private final CCloudApi cli;
   private final String clusterId;
   private final Configuration config;
-  private Map<String, Long> serviceAccountIdByNameMap;
+  private CCloudUtils cCloudUtils;
 
   public CCloudAclsProvider(
       final TopologyBuilderAdminClient adminClient, final Configuration config) {
@@ -32,21 +30,24 @@ public class CCloudAclsProvider extends SimpleAclsProvider implements AccessCont
     this.cli = new CCloudApi(config.getConfluentCloudClusterUrl(), config);
     this.clusterId = config.getConfluentCloudClusterId();
     this.config = config;
+    this.cCloudUtils = new CCloudUtils(config);
   }
 
   @Override
   public void createBindings(Set<TopologyAclBinding> bindings) throws IOException {
-    this.serviceAccountIdByNameMap = initializeLookupTable(this.cli);
+    var serviceAccountIdByNameMap = cCloudUtils.initializeLookupTable(this.cli);
     for (TopologyAclBinding binding : bindings) {
-      cli.createAcl(clusterId, translateIfNecessary(binding));
+      cli.createAcl(
+          clusterId, cCloudUtils.translateIfNecessary(binding, serviceAccountIdByNameMap));
     }
   }
 
   @Override
   public void clearBindings(Set<TopologyAclBinding> bindings) throws IOException {
+    var serviceAccountIdByNameMap = cCloudUtils.initializeLookupTable(this.cli);
     for (TopologyAclBinding binding : bindings) {
-      this.serviceAccountIdByNameMap = initializeLookupTable(this.cli);
-      cli.deleteAcls(clusterId, translateIfNecessary(binding));
+      cli.deleteAcls(
+          clusterId, cCloudUtils.translateIfNecessary(binding, serviceAccountIdByNameMap));
     }
   }
 
@@ -66,65 +67,5 @@ public class CCloudAclsProvider extends SimpleAclsProvider implements AccessCont
       LOGGER.warn(e);
       return Collections.emptyMap();
     }
-  }
-
-  private TopologyAclBinding translateIfNecessary(TopologyAclBinding binding) throws IOException {
-
-    if (!config.isConfluentCloudServiceAccountTranslationEnabled()) {
-      LOGGER.debug("Confluent Cloud Principal translation is currently disabled");
-      return binding;
-    }
-
-    LOGGER.info(
-        "At the time of this PR, 4 Feb the Confluent Cloud ACL(s) api require to translate "
-            + "Service Account names into ID(s). At some point in time this will not be required anymore, "
-            + "so you can configure this out by using ccloud.service_account.translation.enabled=false (true by default)");
-
-    ConfluentCloudPrincipal principal = ConfluentCloudPrincipal.fromString(binding.getPrincipal());
-    long numericServiceAccountId =
-        serviceAccountIdByNameMap.getOrDefault(
-            principal.getServiceAccountName(), SERVICE_ACCOUNT_NOT_FOUND);
-    if (numericServiceAccountId
-        == SERVICE_ACCOUNT_NOT_FOUND) { // Translation failed, so we can't continue
-      throw new IOException(
-          "Translation of principal "
-              + principal
-              + " failed, please review your system configuration");
-    }
-
-    LOGGER.debug(
-        "Translating Confluent Cloud principal "
-            + binding.getPrincipal()
-            + " to "
-            + principal.getPrincipalType().name()
-            + ":"
-            + numericServiceAccountId);
-    TopologyAclBinding translatedBinding =
-        TopologyAclBinding.build(
-            binding.getResourceType(),
-            binding.getResourceName(),
-            binding.getHost(),
-            binding.getOperation(),
-            principal.toMappedPrincipalString(numericServiceAccountId),
-            binding.getPattern());
-    return translatedBinding;
-  }
-
-  private Map<String, Long> initializeLookupTable(CCloudApi cli) throws IOException {
-    Map<String, Long> lookupServiceAccountTable = new HashMap<>();
-
-    Map<String, String> lookupSaName = new HashMap<>();
-    var v2ServiceAccounts = cli.listServiceAccounts();
-    for (var serviceAccount : v2ServiceAccounts) {
-      lookupSaName.put(serviceAccount.getId(), serviceAccount.getName());
-    }
-    var v1ServiceAccounts = cli.listServiceAccountsV1();
-    for (var serviceAccount : v1ServiceAccounts) {
-      var serviceAccountNameOptional =
-          Optional.ofNullable(lookupSaName.get(serviceAccount.getResource_id()));
-      serviceAccountNameOptional.ifPresent(
-          name -> lookupServiceAccountTable.put(name, serviceAccount.getId()));
-    }
-    return lookupServiceAccountTable;
   }
 }
