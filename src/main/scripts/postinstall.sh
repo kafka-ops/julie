61fee7524d72af73063e47c5a1173fe9621f821b
Fobhep <b.hopfenmueller@mosfet.uber.space>
2022-07-29 17:09:34 +0200

    Add autocomplete for bash (#500)
    Co-authored-by: Bernhard Hopfenmüller <hopfenmueller@atix.de>

--- src/main/scripts/postinstall.sh
+++ src/main/scripts/postinstall.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 ln -s /usr/local/julie-ops/bin/julie-ops-cli.sh /usr/bin/julie-ops
+ln -s /usr/local/julie-ops/bin/julie-ops-complete.sh /etc/bash_completion.d/julie-ops-complete
+
 chown julie-kafka.julie-kafka /usr/local/julie-ops/bin/julie-ops-cli.sh
+chown julie-kafka.julie-kafka /usr/local/julie-ops/bin/julie-ops-complete.sh
