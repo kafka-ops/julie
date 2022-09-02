7f969b8bad7918cd332d1409a4305aaa04323706
Tim te Beek <tim.te.beek@jdriven.com>
2022-09-02 21:01:32 +0200

    Add lenient stubbing to TopologyBuilderAdminClientTest

--- src/test/java/com/purbon/kafka/topology/TopologyBuilderAdminClientTest.java
+++ src/test/java/com/purbon/kafka/topology/TopologyBuilderAdminClientTest.java
@@ -28,8 +28,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class TopologyBuilderAdminClientTest {
 
   @Mock CreateAclsResult createAclsResult;
