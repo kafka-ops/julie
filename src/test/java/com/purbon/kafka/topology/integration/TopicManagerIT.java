39045a03a6347da0e759799bf3864a9c61b84c18
Tim te Beek <tim.te.beek@jdriven.com>
2022-09-18 10:03:21 +0200

    Remove unused MockitoExtension

--- src/test/java/com/purbon/kafka/topology/integration/TopicManagerIT.java
+++ src/test/java/com/purbon/kafka/topology/integration/TopicManagerIT.java
@@ -46,10 +46,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 class TopicManagerIT {
 
   private static SaslPlaintextKafkaContainer container;
