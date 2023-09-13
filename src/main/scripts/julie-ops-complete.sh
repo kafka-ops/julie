b1e113ede03de762e159aa11319cc6a837428230
Pere Urbón <purbon@users.noreply.github.com>
2023-09-13 10:10:03 +0200

    Update src/main/scripts/julie-ops-complete.sh
    Co-authored-by: Sverre H. Huseby <shh+github@thathost.com>

--- src/main/scripts/julie-ops-complete.sh
+++ src/main/scripts/julie-ops-complete.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 function __julie-ops () {
 	local base_ops="--brokers|--clientConfig|--dryRun|--help|--overridingClientConfig|--plans|--quite|--topology|--validate|--version"
         local cur=${COMP_WORDS[COMP_CWORD]}
